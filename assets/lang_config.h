848a176a07391a1bd45416eb49e37a0e08349fc3
Ritika090323 <ritikagarg0903@gmail.com>
2026-01-10 23:07:22 -0800

    Initial commit of HackStorm project

---
+++ assets/lang_config.h
@@ -0,0 +1,40 @@
+// Auto-generated language config
+#ifndef __LANGUAGE_CONFIG_H__
+#define __LANGUAGE_CONFIG_H__
+
+#include <stdio.h>
+
+#ifndef en_us
+    #define en_us
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define LANG_CODE "en-US"
+
+#define VERSION "Version "
+#define INITIALIZING "Initializing..."
+#define REGISTERING_NETWORK "Joining Network..."
+#define CONNECT_SERVER "Connecting Cloud..."
+#define STANDBY "Standby"
+#define CONNECT_TO "Connect to "
+#define CONNECTING "Connecting..."
+#define CONNECTED_TO "Connected to "
+#define LISTENING "Listening..."
+#define SPEAKING "Speaking..."
+#define HOLD_TALK "Hold to Talk"
+#define TRIG_TALK "Press to Talk"
+#define WAKEUP_TALK "Wake Word"
+#define FREE_TALK "Free Chat"
+#define ENTERING_WIFI_CONFIG_MODE "Wifi Config Mode..."
+#define VOLUME "Volume "
+#define MUTED "Muted"
+#define MAX_VOLUME "Max Volume"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __LANGUAGE_CONFIG_H
